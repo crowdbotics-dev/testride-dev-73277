94a80185a319ed56a9b845df92353a3233500cdb
Iiro Lehtinen <iiro@crowdbotics.com>
2023-04-07 13:53:30 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "testride-dev-73277",
-  url: "https://testride-dev-73277.botics.co",
-  api: "https://testride-dev-73277.botics.co/api/v1"
-}
+  "name": "testride-dev-73277",
+  "url": "https://testride-dev-73277.botics.co",
+  "api": "https://testride-dev-73277.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
